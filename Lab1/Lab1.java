993c866609f680129f2862b26fe55ae57d700432
maxzaremba <50717781+maxzaremba@users.noreply.github.com>
2019-05-16 16:38:35 -0400

    Add files via upload

---
+++ Lab1/Lab1.java
@@ -0,0 +1,18 @@
+/* Max Zaremba
+ * 2/7/2019
+ * File Name: Lab1
+ * Description: Displays my name with a border and
+ * displays total time spent in this lab.
+ */
+
+public class Lab1{
+   
+   public static void main(String[] args){
+      
+      System.out.println("*****************");
+      System.out.println("*  Max Zaremba  *");
+      System.out.println("*****************");
+      System.out.println("The time I took to finish this lab was" + " " + 20*60 + " " + "seconds.");
+   }
+   
+}
